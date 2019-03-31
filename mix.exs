58abe4e796c39722ad6890c60988974bc716a7ac
yangsoosong <song.yang@husky.neu.edu>
2019-03-31 15:53:18 -0400

    Attempt to add password feature

--- mix.exs
+++ mix.exs
@@ -43,6 +43,7 @@ defmodule Weatherdrobe.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
+
       {:comeonin, "~> 4.1"},
       {:argon2_elixir, "~> 1.3"}
     ]
--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -13,6 +13,8 @@
 alias Weatherdrobe.Repo
 alias Weatherdrobe.Users.User
 
-Repo.insert!(%User{email: "alice@example.com", password: "pwhash", zipcode: "02115"})
-Repo.insert!(%User{email: "bob@example.com", password: "pwhash", zipcode: "99501"})
+pwhash = ARgon2.hash_pwd_salt("password")
+
+Repo.insert!(%User{email: "alice@example.com", password: pwhash, zipcode: "02115"})
+Repo.insert!(%User{email: "bob@example.com", password: pwhash, zipcode: "99501"})
 
