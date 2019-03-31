5f6d5e7636a378a0658f8dfd19080a2979c40222
colbycamp <camp.c@husky.neu.edu>
2019-03-31 12:26:57 -0400

    Started UI

--- config/dev.exs
+++ config/dev.exs
@@ -68,8 +68,8 @@ config :phoenix, :plug_init_mode, :runtime
 
 # Configure your database
 config :weatherdrobe, Weatherdrobe.Repo,
-  username: "weatherdrobe",
-  password: "weatherdrobe",
+  username: "postgres",
+  password: "postgres",
   database: "weatherdrobe_dev",
   hostname: "localhost",
   pool_size: 10
