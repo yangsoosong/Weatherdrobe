da9637c27a447316f095cfc2a242eaab509e2808
yangsoosong <song.yang@husky.neu.edu>
2019-04-07 00:27:06 -0400

    password issue

--- lib/weatherdrobe/users/user.ex
+++ lib/weatherdrobe/users/user.ex
@@ -7,13 +7,53 @@ defmodule Weatherdrobe.Users.User do
     field :password_hash, :string
     field :zipcode, :string
 
+
+    field :password, :string, virtual: true
+    field :password_confirmation, :string, virtual: true
+
     timestamps()
   end
-
+ 
   @doc false
+  
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:email, :password, :password_confirmation])
+    |> validate_confirmation(:password)
+    |> validate_password(:password)
+    |> put_pass_hash()
+    |> validate_required([:email, :password_hash])
+  end
+
+# Password validation
+# From Comeonin docs
+def validate_password(changeset, field, options \\ []) do
+validate_change(changeset, field, fn _, password ->
+case valid_password?(password) do
+{:ok, _} -> []
+{:error, msg} -> [{field, options[:message] || msg}]
+end
+end)
+end
+
+def put_pass_hash(%Ecto.Changeset{
+valid?: true, changes: %{password: password}} = changeset) do
+change(changeset, Comeonin.Argon2.add_hash(password))
+end
+def put_pass_hash(changeset), do: changeset
+
+def valid_password?(password) when byte_size(password) > 4 do
+{:ok, password}
+end
+def valid_password?(_), do: {:error, "The password is too short"}
+end
+
+'''
+@doc false
   def changeset(user, attrs) do
     user
     |> cast(attrs, [:email, :password_hash, :zipcode])
     |> validate_required([:email, :password_hash, :zipcode])
   end
-end
+  end
+  '''
