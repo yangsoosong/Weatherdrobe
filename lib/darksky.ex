ad1b771ab8faf14221bf8f8e5448afde5d2d7d2e
Cheryl <cheryldownie94@gmail.com>
2019-04-07 14:59:29 -0400

    Geocoding and attribution for darksky elixir

--- lib/darksky.ex
+++ lib/darksky.ex
@@ -1,12 +1,16 @@
+@doc """
+  Attribution to github user vegahu for use of forcast funtion found at https://github.com/vegahu/darksky-elixir/blob/master/lib/darksky.ex 
+  """
+
 defmodule Darksky do
 
   @base_url "https://api.darksky.net/forecast/"
 
   @doc """
   Returns the current forecast for the next week
-"""
+  """
   def forecast(api_key, latitude, longitude, opts \\ %{}) when is_map(opts) do
-    forecast = HTTPotion.get("#{@base_url}#{api_key}/#{latitude},#{longitude}", [query: opts])
+    forecast = HTTPotion.get("#{@base_url}#{api_key}/#{latitude},#{longitude}?exclude=minutely,hourly,alerts,flags", [query: opts])
     JSON.decode(forecast.body)
   end
 
