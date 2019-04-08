57a33724015ae8a06695bbd32f6473ec0cf8a6ca
yangsoosong <song.yang@husky.neu.edu>
2019-04-08 08:45:21 +0000

    Deploying shit

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ config :weatherdrobe, WeatherdrobeWeb.Endpoint,
   server: true,
   http: [:inet6, port: System.get_env("PORT") || 4000],
 #  url: [host: "example.com", port: 80],
-  url: [host: "weatherdrobe.yangsoosong.com", port: 80],
+  url: [scheme: "https", host: "weatherdrobe.yangsoosong.com", port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
